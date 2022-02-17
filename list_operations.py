533a851c65f1ae91c9b87c77c5881c4c8328cacf
Vilas Patil <vilaspatil198@gmail.com>
2022-02-17 22:17:14 +0530

    Initial commit

---
+++ list_operations.py
@@ -0,0 +1,16 @@
+"""List operation program"""
+
+list1 = [2,3,12,23,1]
+list1.reverse()
+print("Reverse list is ->" ,list1)
+
+list2 = [2,3,12,23,1]
+list2.sort()
+print("Reverse list is ->" ,list2)
+
+list3 = []
+list3 = list1.copy()
+print("Reverse list is ->" ,list3)
+
+indexvalue = list2[2:5]
+print("The index values are ",indexvalue)
